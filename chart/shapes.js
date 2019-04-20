30d024f1163b3dcb8fff65f5f0f9f92f5aec0c04
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2019-04-20 13:39:09 +0200

    added Ellipse and Lines svg

--- chart/shapes.js
+++ chart/shapes.js
@@ -1,5 +1,8 @@
 const divRectSvg = document.getElementById("rectSvg");
-const divCirclevg = document.getElementById("circleSvg");
+const divCircleSvg = document.getElementById("circleSvg");
+const divEllipseSvg = document.getElementById("ellipseSvg");
+const divLinesSvg = document.getElementById("linesSvg");
+
 const dataArray = [28, 35, 55, 24, 13, 65];
 
 const editedDataArray = [{ value: 28, color: "purple" }, { value: 35, color: "green" }, { value: 55, color: "purple" }];
@@ -26,14 +29,14 @@ rectSvg
 
 // Circle SVG
 const circleSvg = d3
-  .select(divCirclevg)
+  .select(divCircleSvg)
   .append("svg")
   .attr("height", () => {
     return Math.max(...dataArray) * 2 + 50;
   })
   .attr("width", "100%");
 
-let fixedX = 50;
+let circleFixedX = 50;
 circleSvg
   .selectAll("circle")
   .data(dataArray)
@@ -41,14 +44,66 @@ circleSvg
   .append("circle")
   .attr("cx", (d, i) => {
     const firstCx = dataArray[0];
-    const otherCx = fixedX;
+    const otherCx = circleFixedX;
     if (i === 0) {
       return firstCx;
     } else {
-      fixedX += 2 * d + 25;
+      circleFixedX += 2 * d + 25;
       return otherCx + d + 25;
     }
   })
   .attr("cy", (d, i) => Math.max(...dataArray) + 25)
   .attr("r", (d, i) => d)
   .attr("fill", "#BF5967");
+
+// Ellipse SVG
+const ellipseSvg = d3
+  .select(divEllipseSvg)
+  .append("svg")
+  .attr("height", () => {
+    return Math.max(...dataArray) * 2 + 50;
+  })
+  .attr("width", "100%");
+
+let ellipseFixedX = 50;
+ellipseSvg
+  .selectAll("ellipse")
+  .data(dataArray)
+  .enter()
+  .append("ellipse")
+  .attr("cx", (d, i) => {
+    const firstCx = dataArray[0];
+    const otherCx = ellipseFixedX;
+    if (i === 0) {
+      return firstCx;
+    } else {
+      ellipseFixedX += 2 * d + 25;
+      return otherCx + d + 25;
+    }
+  })
+  .attr("cy", (d, i) => Math.max(...dataArray) + 25)
+  .attr("rx", (d, i) => d)
+  .attr("ry", 20)
+  .attr("fill", "#FFD7D6");
+
+// Lines SVG
+const linesSvg = d3
+  .select(divLinesSvg)
+  .append("svg")
+  .attr("height", () => {
+    return Math.max(...dataArray) * 2 + 30;
+  })
+  .attr("width", "100%");
+
+let linesFixedX = 0;
+linesSvg
+  .selectAll("line")
+  .data(dataArray)
+  .enter()
+  .append("line")
+  .attr("x1", linesFixedX)
+  .attr("y1", (d, i) => 30 + i * 20)
+  .attr("x2", (d, i) => d * 3)
+  .attr("y2", (d, i) => 30 + i * 20)
+  .style("stroke", "#703121") // style win to .attr
+  .style("stroke-width", 4);
