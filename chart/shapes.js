8ca76b679b09f02763e2acdeafb1e3091318918c
Laura Marina Bianchi <lauramarinabianchi@gmail.com>
2019-04-20 11:50:06 +0200

    added circle svg :large_blue_circle:

--- chart/shapes.js
+++ chart/shapes.js
@@ -22,13 +22,33 @@ rectSvg
   .attr("width", 80)
   .attr("x", (d, i) => 90 * i)
   .attr("y", (d, i) => 200 - d * 2)
-  .attr("fill", "purple");
+  .attr("fill", "#97A676");
 
 // Circle SVG
 const circleSvg = d3
   .select(divCirclevg)
   .append("svg")
   .attr("height", () => {
-    return Math.max(...dataArray) * 2 + 70;
+    return Math.max(...dataArray) * 2 + 50;
   })
   .attr("width", "100%");
+
+let fixedX = 50;
+circleSvg
+  .selectAll("circle")
+  .data(dataArray)
+  .enter()
+  .append("circle")
+  .attr("cx", (d, i) => {
+    const firstCx = dataArray[0];
+    const otherCx = fixedX;
+    if (i === 0) {
+      return firstCx;
+    } else {
+      fixedX += 2 * d + 25;
+      return otherCx + d + 25;
+    }
+  })
+  .attr("cy", (d, i) => Math.max(...dataArray) + 25)
+  .attr("r", (d, i) => d)
+  .attr("fill", "#BF5967");
